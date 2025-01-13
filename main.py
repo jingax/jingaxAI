5be4b8317a791d349bdb914fb6b3cf6b61d819bb
Aastik Solanki <52195872+jingax@users.noreply.github.com>
2025-01-13 20:16:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -103,7 +103,7 @@ if prompt := st.chat_input("Ask me anything about Aastik",disabled=st.session_st
         response = f"You get to ask only {q_count} questions!"
     # Display assistant response in chat message container
     with st.chat_message("assistant"):
-        st.markdown(response)
+        st.markdown(response, unsafe_allow_html=True)
     # Add assistant response to chat history
     st.session_state.messages.append({"role": "assistant", "content": response})
     st.session_state.logger.info(f"{st.session_state.curr_id}::jinga-ai:: {response}")
